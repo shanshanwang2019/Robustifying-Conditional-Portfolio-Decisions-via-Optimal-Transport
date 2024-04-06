d3f46ab85ddabcbef01243f7c9dc7cd59608c09d
Shanshan Wang <67844613+shanshanwang2019@users.noreply.github.com>
2024-04-06 21:34:03 +0800

    Add files via upload

--- mean-variance/modeling/factors/__init__.py
+++ mean-variance/modeling/factors/__init__.py
@@ -1,1 +1,13 @@
+__all__ = [
+    "gen_china_fama_french_factors_df",
+    "gen_US_fama_french_factors_df",
+    "read_china_fama_french_factors_df",
+    "read_US_fama_french_factors_df",
+]
 
+from .fama_french import (
+    gen_china_fama_french_factors_df,
+    gen_US_fama_french_factors_df,
+    read_china_fama_french_factors_df,
+    read_US_fama_french_factors_df,
+)
